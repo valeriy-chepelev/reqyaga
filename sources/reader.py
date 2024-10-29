53154c4bca259bb8aae9ba6458b5ceb6c32169fe
valeriy chepelev <valeriychepelev@gmail.com>
2024-10-30 00:30:40 +0300

    login options, headless, scripts wait

--- sources/reader.py
+++ sources/reader.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import configparser
 from time import sleep
 
@@ -18,9 +20,15 @@ def read_config(filename):
     return config['DEFAULT']
 
 
-def _wait(w_driver):
-    WebDriverWait(driver=w_driver, timeout=20).until(
+def _wait_page(drv):
+    WebDriverWait(driver=drv, timeout=20).until(
         lambda x: x.execute_script("return document.readyState === 'complete'"))
+
+
+def _wait_data(drv):
+    element_present = EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.TransformedHtml.PageDoc-Content.PageDoc-Content_type_wysiwyg'))  # tuple required
+    WebDriverWait(drv, 10).until(element_present)
 
 
 def login(config):
@@ -29,31 +37,51 @@ def login(config):
     :param config: dictionary with credentials 'login', 'password', 'checkin'
     :return: webdriver with logged-in Wiki root page
     """
+
+    # Create Chrome Options and Service
     service = Service(executable_path='chromedriver.exe')
-    drv = webdriver.Chrome(service=service)
+    options = Options()
+    options.add_argument("--headless")  # Run in headless mode
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple"
+                         "WebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36")
+    options.add_argument("--disable-extensions")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--password-store=basic")
+    options.add_argument("--no-sandbox")
+
+    # Start Chrome Driver
+    drv = webdriver.Chrome(service=service, options=options)
 
     # enter login page
     drv.get("https://wiki.yandex.ru/?skipPromo=1")
-    _wait(drv)
+    _wait_page(drv)
+
     # select login using Yandex ID
     drv.find_element(By.CSS_SELECTOR,
                      "button.g-button.g-button_view_action.g-button_size_l."
                      "g-button_pin_round-round.auth-button.login-form__button").click()
-    _wait(drv)
+    _wait_page(drv)
+
     # enter credentials
     entry = drv.find_element(By.CSS_SELECTOR, "#passp-field-login")
     entry.send_keys(config['login'])
     entry.submit()
-    _wait(drv)
+    _wait_page(drv)
     sleep(1)
     entry = drv.find_element(By.CSS_SELECTOR, "#passp-field-passwd")
     entry.send_keys(config['password'])
     entry.submit()
-    _wait(drv)
-    sleep(
-        5)  # TODO: wait for page loaded, see https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python
+    _wait_data(drv)
+
+    # check wiki root page loaded
     assert config['checkin'] in drv.page_source
+
     return drv
+
+
+def tables(drv):
+    t = drv.find_elements(By.XPATH, '//table')
+    print(f"{len(t)} tables found.")
 
 
 cfg = read_config('reqyaga.ini')
@@ -61,5 +89,6 @@ driver = login(cfg)
 # open data page
 driver.get("https://wiki.yandex.ru/mt/mt-rd/ff-e710f1/prorabotka-konstruktiva/"
            "1.4.-moduli-binarnyx-signalov-mbs-ili-vxodovvyxodo/2.4.1.-trebovanija-k-mvv/")
-_wait(driver)
+_wait_data(driver)
+tables(driver)
 input('Press enter')
